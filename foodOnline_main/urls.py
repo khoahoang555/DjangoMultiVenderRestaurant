344b7376a47dfc80cd34b54e90feb716e4350f81
khoa pham <khoa.pham@tpssoft.com>
2025-10-06 18:28:30 +0700

    location based search functionality

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path('marketplace/', include('marketplace.urls')),
     # CART
     path('cart/', MarketplaceViews.cart, name='cart'),
+    # Search
+    path('search/', MarketplaceViews.search, name='search'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
